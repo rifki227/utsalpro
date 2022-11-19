e75ef1b10c48cd9dd61fd46dd9245a4b758b36fd
rifki227 <115854733+rifki227@users.noreply.github.com>
2022-11-19 09:34:36 +0700

    Add files via upload

---
+++ variabel.java
@@ -0,0 +1,12 @@
+package alpro;
+
+public class variabel {
+	public static void main(String[] args) {
+        String nama = "Rifki Arif Rahman";
+        int tahun = 2003;
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Saya lahir di tahun : " + tahun);
+    }
+}
+
